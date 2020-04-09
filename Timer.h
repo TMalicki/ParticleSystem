119ef306ebcc414b0b0791f70ea4e1573ddb6ab3
Tomasz Malicki <t.malicki1@wp.pl>
2020-04-09 18:44:40 +0200

    added particleExplode()
    Rework of particle class. Now it is managed from ParticleManage class. Added particleExplode() method.

--- Timer.h
+++ Timer.h
@@ -2,11 +2,36 @@
 #include <iostream>
 #include <chrono>
 
+static std::vector<float> s_time (1'000);
+static int counter { 0 };
+
 auto getTime(std::chrono::high_resolution_clock::time_point& start) 
 {
 	auto stop = std::chrono::high_resolution_clock::now();
-	auto timePassed = std::chrono::duration<double, std::milli> (stop - start);
+	auto timePassed = std::chrono::duration<float, std::milli> (stop - start);
 	start = stop;
+
+	if (counter == s_time.size()) counter = 0;
+	s_time.at(counter) = timePassed.count();
+	counter++;
 
 	return  timePassed.count();
 }
+
+const auto& timePlot()
+{
+	return s_time;
+}
+
+/*
+class Timer
+{
+private:
+	//std::vector<float> m_time;
+public:
+	Timer() {};
+	auto updateTime(std::chrono::high_resolution_clock::time_point&);
+	inline auto startTime() { return std::chrono::high_resolution_clock::now(); }
+	//const auto& timePlot(float dt);
+};
+*/
